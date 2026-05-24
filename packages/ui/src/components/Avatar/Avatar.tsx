c7deb528fa24adc8173d445cabe5acb2b5083a6e
Sergey Vrenev <blinks44@yandex.ru>
2026-05-24 11:01:11 +0300

    feat(avatar): finish updates

--- packages/ui/src/components/Avatar/Avatar.tsx
+++ packages/ui/src/components/Avatar/Avatar.tsx
@@ -1,4 +1,5 @@
 import { Avatar as AvatarPrimitive } from '@base-ui/react/avatar';
+import * as React from 'react';
 import { mergeClassName } from '@/utils/mergeClassName';
 import styles from './Avatar.module.css';
 
@@ -9,43 +10,54 @@ type AvatarProps = AvatarPrimitive.Root.Props & {
 };
 type AvatarState = AvatarPrimitive.Root.State;
 
-function Avatar({ className, size = 'md', ...props }: AvatarProps) {
-  return (
-    <AvatarPrimitive.Root
-      data-slot="avatar-root"
-      data-size={size}
-      className={mergeClassName(className, styles.root)}
-      {...props}
-    />
-  );
-}
+const Avatar = React.forwardRef<React.ComponentRef<typeof AvatarPrimitive.Root>, AvatarProps>(
+  function Avatar({ className, size = 'md', ...props }, ref) {
+    return (
+      <AvatarPrimitive.Root
+        ref={ref}
+        data-slot="avatar-root"
+        data-size={size}
+        className={mergeClassName(className, styles.root)}
+        {...props}
+      />
+    );
+  },
+);
 
 type AvatarImageProps = AvatarPrimitive.Image.Props;
 type AvatarImageState = AvatarPrimitive.Image.State;
 
-function AvatarImage({ className, ...props }: AvatarImageProps) {
+const AvatarImage = React.forwardRef<
+  React.ComponentRef<typeof AvatarPrimitive.Image>,
+  AvatarImageProps
+>(function AvatarImage({ className, ...props }, ref) {
   return (
     <AvatarPrimitive.Image
+      ref={ref}
       data-slot="avatar-image"
       className={mergeClassName(className, styles.image)}
       {...props}
     />
   );
-}
+});
 
 type AvatarFallbackProps = AvatarPrimitive.Fallback.Props;
 type AvatarFallbackState = AvatarPrimitive.Fallback.State;
 type AvatarImageLoadingStatus = AvatarImageState['imageLoadingStatus'];
 
-function AvatarFallback({ className, ...props }: AvatarFallbackProps) {
+const AvatarFallback = React.forwardRef<
+  React.ComponentRef<typeof AvatarPrimitive.Fallback>,
+  AvatarFallbackProps
+>(function AvatarFallback({ className, ...props }, ref) {
   return (
     <AvatarPrimitive.Fallback
+      ref={ref}
       data-slot="avatar-fallback"
       className={mergeClassName(className, styles.fallback)}
       {...props}
     />
   );
-}
+});
 
 export { Avatar, AvatarImage, AvatarFallback };
 
