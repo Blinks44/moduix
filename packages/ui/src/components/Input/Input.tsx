ba561a87637b3cf0e2ddb1fb6ef90031aa6b9206
Sergey Vrenev <blinks44@yandex.ru>
2026-05-28 02:22:50 +0300

    feat(input): polishing api

--- packages/ui/src/components/Input/Input.tsx
+++ packages/ui/src/components/Input/Input.tsx
@@ -1,15 +1,16 @@
+import type { ComponentProps, ComponentRef } from 'react';
 import { Input as InputPrimitive } from '@base-ui/react/input';
-import * as React from 'react';
+import { forwardRef } from 'react';
 import { mergeClassName } from '@/utils/mergeClassName';
 import styles from './Input.module.css';
 
 type InputVisualSize = 'xs' | 'sm' | 'md' | 'lg' | 'xl';
 
-const Input = React.forwardRef<
-  React.ComponentRef<typeof InputPrimitive>,
+const Input = forwardRef<
+  ComponentRef<typeof InputPrimitive>,
   Omit<InputPrimitive.Props, 'size'> & {
     size?: InputVisualSize;
-    htmlSize?: React.ComponentProps<'input'>['size'];
+    htmlSize?: ComponentProps<'input'>['size'];
   }
 >(function Input({ className, size = 'md', htmlSize, ...props }, ref) {
   return (
